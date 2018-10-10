9bec7e2b566b48a23cce6e4618041c7e83b91e89
AlexandrD <alexandr_do@mail.ru>
2018-10-10 12:37:59 +0300

    added GenresCest

---
+++ frontend/tests/functional/GenresCest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace frontend\tests\functional;
+
+use frontend\tests\FunctionalTester;
+
+class GenresCest
+{
+    public function checkOpen(FunctionalTester $I)
+    {
+        $I->amOnPage('genres');
+        $I->see('Genres', 'h1');
+        $I->seeLink('Create Genres');
+        $I->seeLink('ID');
+        $I->seeLink('Genre Name');
+
+    }
+
+    public function checkCreateGenresPage(FunctionalTester $I)
+    {
+        $I->amOnPage('genres/create');
+        $I->see('Create Genres', 'h1');
+    }
+
+    public function checkCreateGenresEmptyFields(FunctionalTester $I)
+    {
+        $I->amOnPage('genres/create');
+        $I->click('Save');
+        $I->seeValidationError('Genre Name cannot be blank.');
+    }
+
+    public function checkCreateGenresNotEmpty(FunctionalTester $I)
+    {
+        $I->amOnPage('genres/create');
+        $I->fillField('Genre Name', 'sdadasdasd');
+        $I->click('Save');
+        $I->see('Genre Name');
+        $I->see('ID');
+        $I->see('sdadasdasd');
+        $I->seeLink('Update');
+        $I->seeLink('Delete');
+    }
+
+    public function checkUpdateGenres(FunctionalTester $I)
+    {
+        $I->amOnPage('genres/create');
+        $I->fillField('Genre Name', 'sdadasdasd');
+        $I->click('Save');
+        $I->click('Update');
+        $I->see('Update Genres', 'h1');
+        $I->fillField('Genre Name', 'fffffff');
+        $I->click('Save');
+        $I->see('fffffff');
+
+    }
+}
